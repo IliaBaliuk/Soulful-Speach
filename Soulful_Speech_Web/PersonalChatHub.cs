125035d1a390e1b2c35d19239a850a57811a3f7e
Илья <poker01@i.ua>
2021-06-04 06:02:34 +0300

    Added Send method to PersonalChatHub

--- Soulful_Speech_Web/PersonalChatHub.cs
+++ Soulful_Speech_Web/PersonalChatHub.cs
@@ -8,5 +8,9 @@ namespace Soulful_Speech_Web
 {
     public class PersonalChatHub : Hub
     {
+        public async Task Send(string userId, string message, string userName, string dateTime)
+        {
+            await Clients.User(userId).SendAsync("Send", message, userName, dateTime);
+        }
     }
 }
